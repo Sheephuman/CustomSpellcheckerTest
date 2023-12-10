3525df844cc31c1258bcc6d3d7b0d8cdc72d09b9
sheephuman <shin.citysheep@gmail.com>
2023-12-10 13:25:49 +0900

    Edit Classの追加

--- SpellcheckerTest/Edit.cs
+++ SpellcheckerTest/Edit.cs
@@ -1,6 +1,11 @@
 ﻿namespace SpellcheckerTest
 {
-    internal class Edit
-    {
-    }
+        public class Edit
+        {
+            public int Position { get; set; }  // 編集が行われた位置
+
+            public string InsertedText { get; set; }  // 挿入されたテキスト
+
+            public string DeletedText { get; set; }  // 削除されたテキスト
+        }
 }
--- SpellcheckerTest/ITextBuffer.cs
+++ SpellcheckerTest/ITextBuffer.cs
@@ -1,5 +1,6 @@
-﻿using System.Text;
-using Microsoft.VisualStudio.Text;
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Text; //use Nuget Package "MicroSoft.VisualStudio.Text.Data"
 
 namespace SpellcheckerTest
 {
@@ -7,6 +8,35 @@ namespace SpellcheckerTest
     public class CustomTextBuffer : ITextBuffer
     {
         private StringBuilder buffer;
+        public CustomTextBuffer()
+        {
+            buffer = new StringBuilder();
+        }
+
+    }
+
+    public class CustomTextEdit : ITextEdit
+    {
+        private CustomTextBuffer textBuffer;
+        private List<Edit> edits = new List<Edit>();
+
+        public CustomTextEdit(CustomTextBuffer buffer)
+        {
+            textBuffer = buffer;
+        }
+
+    }
+
+    public class CustomTextSnapshot : ITextSnapshot
+    {
+        private CustomTextBuffer textBuffer;
+        private string text;
+
+        public CustomTextSnapshot(CustomTextBuffer buffer, string setText)
+        {
+            textBuffer = buffer;
+            this.text = setText;
+        }
 
     }
 }
